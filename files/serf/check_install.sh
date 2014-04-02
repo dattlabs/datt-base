8b2a02e574833e15af0b325c60d5a758bcc1f7fa
Jonathan Leonard <johanatan@gmail.com>
2014-04-01 22:34:39 -0700

    Fix syntax error.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in supervisord supervisorctl inotifywait serf; do
-  command -v $i >/dev/null 2>&1 || { echo >&2 "[FAIL] Program '$i' required, but not installed."; exit 1 }
+  command -v $i &>/dev/null || { echo >&2 "[FAIL] Program '$i' required, but not installed."; exit 1; }
 done
 
 echo >&1 "[OK] Installation check succeeded."; exit 0
