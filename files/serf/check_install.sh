ccf9aee839df0560a40f67ba5472c69e3ad1d925
Jonathan Leonard <johanatan@gmail.com>
2014-04-01 21:31:35 -0700

    Merge.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in supervisor supervisorctl inotifywait serf; do
+for i in supervisord supervisorctl inotifywait serf; do
   command -v $i >/dev/null 2>&1 || { echo >&2 "[FAIL] Program '$i' required, but not installed."; exit 1; }
 done
 
