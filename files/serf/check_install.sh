3e3a46385546efdbf2eaafbc5ff8912f9888c90d
Jonathan Leonard <johanatan@users.noreply.github.com>
2014-04-01 22:28:11 -0700

    Remove unnecessary noise.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-set -e
-
 for i in supervisord supervisorctl inotifywait serf; do
-  command -v $i >/dev/null 2>&1 || { echo >&2 "[FAIL] Program '$i' required, but not installed."; }
+  command -v $i >/dev/null 2>&1 || { echo >&2 "[FAIL] Program '$i' required, but not installed."; exit 1 }
 done
 
-if [[ $? != 0 ]]; then
-  return 1
-else
-  echo >&1 "[OK] Installation check succeeded."
-fi
+echo >&1 "[OK] Installation check succeeded."
+exit 0
