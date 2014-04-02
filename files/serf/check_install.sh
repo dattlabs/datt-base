159da7e35a0491dfda4a54d059256d3760c0e14d
Jonathan Leonard <johanatan@users.noreply.github.com>
2014-04-01 22:29:38 -0700

    Consolidate lines.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -4,5 +4,4 @@ for i in supervisord supervisorctl inotifywait serf; do
   command -v $i >/dev/null 2>&1 || { echo >&2 "[FAIL] Program '$i' required, but not installed."; exit 1 }
 done
 
-echo >&1 "[OK] Installation check succeeded."
-exit 0
+echo >&1 "[OK] Installation check succeeded."; exit 0
