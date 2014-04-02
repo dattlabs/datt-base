22c7902d492244cad3a8a9e6c34bf7d95ff21679
John Albietz <inthecloud247@gmail.com>
2014-04-01 20:58:33 -0700

    better join script. improveddd.

--- files/serf/join.sh
+++ files/serf/join.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-for i in inotifywait serf; do
-  command -v $i >/dev/null 2>&1 || { echo >&2 "[FAIL] $i required, but not installed. Aborting."; exit 1; }
+INSTALL_CHECK=0
+for i in supervisor supervisorctl inotifywait serf; do
+  command -v $i >/dev/null 2>&1 || { echo >&2 "[FAIL] Program '$i' required, but not installed."; INSTALL_CHECK=1; }
 done
+
+if [ $INSTALL_CHECK -eq 1 ]; then
+  echo [FAIL] Serf Join: Missing Dependencies. Exiting.
+  exit 1
+fi
 
 SERF_IP=${AMBASSADOR_PORT_7946_TCP_ADDR:-"127.0.0.1"}
 SERF_PORT=${AMBASSADOR_PORT_7946_TCP_PORT:-"7946"}
-SERF_DIR="/files/serf"
+SERF_DIR=${SERFJOIN_DIR:-"/files/serf"}
 
-echo Looping serf join when files change
 if [[ $1 = "-d" ]]; then
   sleep 1
   echo "[INFO] daemonizing for supervisor"
@@ -22,13 +27,13 @@ if [[ $1 = "-d" ]]; then
       echo "[OK] Serf Command: serf join $SERF_IP:$SERF_PORT"
     fi
     inotifywait -rq $SERF_DIR 2>&1 > /dev/null
+    serf leave && echo "[INFO] serf agent gracefully left the cluster"
   done
 else
-  # run without any flags.
+  # as a non-daemon, join only after serf agent is running
   until ps aux | grep -q "[s]erf agent"; do
     echo "[FAIL] serf agent not running"
     sleep 1
   done
-  echo "[OK] serf agent running.";
-  serf join $SERF_IP:$SERF_PORT
+  serf join $SERF_IP:$SERF_PORT && echo "[OK] serf agent running.";
 fi
