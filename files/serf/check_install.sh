2b7e5db02322293938fc501d527b95c6a415f2a1
Jonathan Leonard <johanatan@gmail.com>
2014-04-01 23:14:08 -0700

    Print all missing dependencies before failing.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for i in supervisord supervisorctl inotifywait serf; do
-  command -v $i &>/dev/null || { echo >&2 "[FAIL] Program '$i' required, but not installed."; exit 1; }
+  command -v $i &>/dev/null || { echo >&2 "[FAIL] Program '$i' required, but not installed."; fail=1; }
 done
 
+if [[ $fail == 1 ]]; then exit 1; fi
 echo >&1 "[OK] Installation check succeeded."; exit 0
