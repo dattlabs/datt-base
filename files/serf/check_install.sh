3e75af6bdcb604a5d1755f0deceef17ec83687f5
Jonathan Leonard <johanatan@gmail.com>
2014-04-01 23:27:27 -0700

    Grammar fix.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -2,7 +2,7 @@
 
 unset fail
 for i in supervisord supervisorctl inotifywait serf; do
-  command -v $i &>/dev/null || { echo >&2 "[FAIL] Program '$i' required, but not installed."; fail=1; }
+  command -v $i &>/dev/null || { echo >&2 "[FAIL] Program '$i' required but not installed."; fail=1; }
 done
 
 if [[ $fail == 1 ]]; then exit 1; fi
