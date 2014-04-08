74b64c56ab23f624317d98a4374f19c1cfc0ebbc
Jonathan Leonard <johanatan@gmail.com>
2014-04-08 13:06:09 -0700

    Factor check_install.sh into re-usable file.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. $SCRIPT_DIR/../scripts/helpers.bash
-
-unset fail
-for i in supervisord supervisorctl inotifywait serf; do
-  [[ `is_installed $i` = "1" ]] || fail=1
-done
-
-[[ $fail == 1 ]] && exit 1
-echo >&1 "[OK] Installation check succeeded."; exit 0
+$SCRIPT_DIR/../scripts/check_install.sh supervisord supervisorctl inotifywait serf
