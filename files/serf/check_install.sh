774320fd67a6f53a1d81223c8438402bf0f0be60
Jonathan Leonard <johanatan@gmail.com>
2014-04-08 12:59:05 -0700

    Let check install use helpers.bash.

--- files/serf/check_install.sh
+++ files/serf/check_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $SCRIPT_DIR/../scripts/helpers.bash
+
 unset fail
 for i in supervisord supervisorctl inotifywait serf; do
-  command -v $i &>/dev/null || { echo >&2 "[FAIL] Program '$i' required but not installed."; fail=1; }
+  [[ `is_installed $i` = "1" ]] || fail=1
 done
 
-if [[ $fail == 1 ]]; then exit 1; fi
+[[ $fail == 1 ]] && exit 1
 echo >&1 "[OK] Installation check succeeded."; exit 0
